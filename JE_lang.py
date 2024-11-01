3af2c7fc4d156b606a36ee609882d7b326951f18
Jingji132 <ruaixin4@aliyun.com>
2024-11-01 10:54:39 +0800

    adjust

--- JE_lang.py
+++ JE_lang.py
@@ -32,7 +32,7 @@ def find_je_lang(game_path=r"E:\Minecraft\.minecraft", target_path=r"D:\Users\Ec
                 lang = json.load(f)
         return lang
 
-    def find_zh_cn(path):
+    def find_lang(path, lang_type='zh_cn'):
         assets_path = os.path.join(path, r"assets")
         indexes_path = os.path.join(assets_path, r"indexes")
         index_num = 0
@@ -42,7 +42,7 @@ def find_je_lang(game_path=r"E:\Minecraft\.minecraft", target_path=r"D:\Users\Ec
                 index_num = num
         indexes_path = os.path.join(indexes_path, str(index_num) + ".json")
         with open(indexes_path, 'r', encoding='utf-8') as f:
-            zh_cn_name = json.load(f)['objects']['minecraft/lang/zh_cn.json']['hash']
+            zh_cn_name = json.load(f)['objects'][f'minecraft/lang/{lang_type}.json']['hash']
         zh_cn_folder = zh_cn_name[0:2]
         zh_cn_path = os.path.join(assets_path, 'objects', zh_cn_folder, zh_cn_name)
         with open(zh_cn_path, 'r', encoding='utf-8') as f:
@@ -54,9 +54,10 @@ def find_je_lang(game_path=r"E:\Minecraft\.minecraft", target_path=r"D:\Users\Ec
         return
     # create_lang(en_us_lang, target_path, "en_us.lang")
 
-    zh_cn_lang = find_zh_cn(game_path)
+    zh_cn_lang = find_lang(game_path)
+    zh_tw_lang = find_lang(game_path, 'zh_tw')
     # create_lang(zh_cn_lang, target_path, "zh_cn.lang")
-    return en_us_lang, zh_cn_lang
+    return en_us_lang, zh_cn_lang, zh_tw_lang
 
 
 def create_lang(lang, path, name):
@@ -84,11 +85,11 @@ def translate_memory(en, zh, _path, name):
         f.writelines(the_list)
 
 
-def translate_memory2(en, zh, _path, name):
-    headers = ['en-US', 'zh-CN']
+def translate_memory2(en, zh, tw, _path, name):
+    headers = ['en-US', 'zh-CN', 'zh-TW']
     rows = []
     for key in zh:
-        rows.append((en[key], zh[key]))
+        rows.append((en[key], zh[key], tw[key]))
     _path = os.path.join(_path, name)
     with open(_path, 'w', encoding='utf-8', newline='') as f:
         writer = csv.writer(f)
@@ -97,8 +98,8 @@ def translate_memory2(en, zh, _path, name):
 
 
 if __name__ == '__main__':
-    en_us, zh_cn = find_je_lang()
+    en_us, zh_cn, zh_tw = find_je_lang()
     path = r"D:\Users\Economy\git\Gitee\MCJE-lang"
     # create_lang(en_us, path, 'en_US.lang')
     # create_lang(zh_cn, path, 'zh_CN.lang')
-    translate_memory2(en_us, zh_cn, path, 'output.csv')
+    translate_memory2(en_us, zh_cn, zh_tw, path, 'output.csv')
--- mclangcn_process.py
+++ mclangcn_process.py
@@ -161,4 +161,4 @@ if __name__ == '__main__':
     pre = True
     # crowdin.download_translate(pre)
     Convert_Lang.crowdin_to_mclangcn_csv(pre)
-    Convert_Lang.crowdin_to_mclangcn_csv(pre, 'zh_TW')
+    # Convert_Lang.crowdin_to_mclangcn_csv(pre, 'zh_TW')
